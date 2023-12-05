80205b4795a97208f7f18ab100da986f5f74ee67
lesley_w <lrwesley92@gmail.com>
2023-12-04 19:05:00 -0600

    Added data as js files

---
+++ src/main-data.js
@@ -0,0 +1,43 @@
+export default [
+  {
+    icon: "icon-facebook.svg",
+    platform: "facebook",
+    username: "@nathanf",
+    total: "1987",
+    type: "followers",
+    increase: true,
+    amountChanged: 12,
+    color: "#198ff5"
+  },
+  {
+    icon: "icon-twitter.svg",
+    platform: "twitter",
+    username: "@nathanf",
+    total: "1044",
+    type: "followers",
+    increase: true,
+    amountChanged: 99,
+    color: "#1ca0f2"
+  },
+  {
+    icon: "icon-instagram.svg",
+    platform: "instagram",
+    username: "@realnathanf",
+    total: "11k",
+    type: "followers",
+    increase: true,
+    amountChanged: 1099,
+    color: "linear-gradient(90deg, #fdc468, #df4996)",
+    gradient: true
+  },
+  {
+    icon: "icon-youtube.svg",
+    platform: "youtube",
+    username: "Nathan F.",
+    total: "8238",
+    type: "subscribers",
+    increase: false,
+    amountChanged: 144,
+    color: "#c4032a"
+  }
+]
---
+++ src/todays-data.js
@@ -0,0 +1,74 @@
+export default [
+  {
+    id: 1,
+    stat: "Page Views",
+    icon: "icon-facebook.svg",
+    platform: "Facebook",
+    total: "87",
+    increase: true,
+    percentChanged: 3
+  },
+  {
+    id: 2,
+    stat: "Likes",
+    icon: "icon-facebook.svg",
+    platform: "Facebook",
+    total: "52",
+    increase: false,
+    percentChanged: 2
+  },
+  {
+    id: 3,
+    stat: "Likes",
+    icon: "icon-instagram.svg",
+    platform: "Instagram",
+    total: "5462",
+    increase: true,
+    percentChanged: 2257
+  },
+  {
+    id: 4,
+    stat: "Profile Views",
+    icon: "icon-instagram.svg",
+    platform: "Instagram",
+    total: "52k",
+    increase: true,
+    percentChanged: 1375
+  },
+  {
+    id: 5,
+    stat: "Retweets",
+    icon: "icon-twitter.svg",
+    platform: "Twitter",
+    total: 117,
+    increase: true,
+    percentChanged: 303
+  },
+  {
+    id: 6,
+    stat: "Likes",
+    icon: "icon-twitter.svg",
+    platform: "Twitter",
+    total: 507,
+    increase: true,
+    percentChanged: 553
+  },
+  {
+    id: 7,
+    stat: "Likes",
+    icon: "icon-youtube.svg",
+    platform: "Youtube",
+    total: "107",
+    increase: false,
+    percentChanged: 19
+  },
+  {
+    id: 8,
+    stat: "Total Views",
+    icon: "icon-youtube.svg",
+    platform: "Youtube",
+    total: "1407",
+    increase: false,
+    percentChanged: 12
+  }
+]

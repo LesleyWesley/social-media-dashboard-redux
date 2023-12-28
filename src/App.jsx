8715bcc49c37a3e8a7ac502a32f1b04a079f0344
lesley_w <lrwesley92@gmail.com>
2023-12-28 12:01:09 -0600

    Added top design element and styled

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import LittleCardSection from "./components/LittleCardSection.jsx"
 function App() {
   return (
     <div className="body-bg">
+      <div className="top-design"></div>
       <div className="container">
         <Header />
         <BigCardSection />
